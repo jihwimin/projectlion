c02683e250c3c9643f4449cd435fb8e06c82f2ae
jihwimin <jihwimin@gmail.com>
2025-02-05 19:17:20 -0500

    update

--- liongram/config/urls.py
+++ liongram/config/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from posts.views import index, url_view, url_parameter_view, function_view, class_view, function_list_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("url/", url_view),
+    path('url/<str:username>/', url_parameter_view),
+    path('fbv/', function_view),
+    path('fbv/list/', function_list_view),
+    path('cbv/', class_view.as_view()),
+    path('', index, name ='index'),
+    path('posts/', include('posts.urls', namespace= 'posts')),
 ]
